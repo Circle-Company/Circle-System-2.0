db48e37aac752f8716323017e689fb5a113c1bb0
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-10-02 14:23:15 -0300

    feat: adicionar DTOs para o caso de uso de registro de usuários

---
+++ src/domain/auth/auth.dtos.ts
@@ -0,0 +1,68 @@
+import { UserProfilePicture, UserStatus, UserTerms } from "@/domain/user/types/user.type"
+export interface SignUpInputDto {
+    username: string
+    password: string
+    metadata: {
+        termsAccepted: boolean
+        forwardedFor: string
+        userAgent?: string
+        machineId?: string
+        timezone?: string
+        latitude?: number
+        longitude?: number
+    }
+}
+
+export interface SignUpOutputDto {
+    session: {
+        user: {
+            id: string
+            username: string
+            name: string
+            description: string
+            profilePicture: Omit<UserProfilePicture, "createdAt" | "updatedAt">
+            status: Omit<UserStatus, "createdAt" | "updatedAt">
+            metrics: {
+                totalLikesReceived: number
+                totalViewsReceived: number
+                totalSharesReceived: number
+                totalCommentsReceived: number
+                totalMemoriesCreated: number
+                totalMomentsCreated: number
+                totalFollowers: number
+                totalFollowing: number
+                lastMetricsUpdate: Date
+            }
+            terms: Omit<UserTerms, "createdAt" | "updatedAt">
+            lastLogin: Date
+            lastPasswordUpdate: Date
+        }
+        preferences: {
+            appTimezone: number
+            language: {
+                appLanguage: string
+                translationLanguage: string
+            }
+            content: {
+                disableAutoplay: boolean
+                disableHaptics: boolean
+                disableTranslation: boolean
+            }
+            pushNotifications: {
+                disableLikeMomentPushNotification: boolean
+                disableNewMemoryPushNotification: boolean
+                disableAddToMemoryPushNotification: boolean
+                disableFollowUserPushNotification: boolean
+                disableViewUserPushNotification: boolean
+                disableNewsPushNotification: boolean
+                disableSugestionsPushNotification: boolean
+                disableAroundYouPushNotification: boolean
+            }
+            creation: {
+                defaultMomentVisibility: "public" | "followers_only" | "private"
+            }
+        }
+        jwtToken: string
+        jwtExpiration: string
+    }
+}
