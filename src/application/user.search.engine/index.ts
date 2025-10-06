a6ec8e2e2371272d0970c51f18de9e0972d8536a
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-10-06 11:55:59 -0300

    refactor: remover arquivos de casos de uso e serviços do motor de busca de usuários para simplificar a estrutura do módulo

--- src/application/user.search.engine/index.ts
+++
@@ -1,7 +0,0 @@
-// Use Cases
-export * from "./use.cases/get.search.suggestions.use.case"
-export * from "./use.cases/search.users.use.case"
-
-// Services
-export * from "./services/cache.service"
-export * from "./services/ranking.service"
