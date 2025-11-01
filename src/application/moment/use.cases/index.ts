5f12b68cc8d5d2a5a59224bf0ee558b807b2599a
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-11-01 18:50:45 -0300

    refactor: remove unused moment use cases for metrics, reports, and analytics to streamline codebase

--- src/application/moment/use.cases/index.ts
+++ src/application/moment/use.cases/index.ts
@@ -13,20 +13,9 @@ export { UnlikeMomentUseCase } from "./unlike.moment.use.case"
 export { GetAccountMomentsUseCase } from "./get.account.moments.use.case"
 export { GetUserMomentsUseCase } from "./get.user.moments.use.case"
 
-// Analytics and metrics
-export { GetMomentMetricsUseCase } from "./get.moment.metrics.use.case"
-export { GetMomentReportsUseCase } from "./get.moment.reports.use.case"
-export { GetMomentsAnalyticsUseCase } from "./get.moments.analytics.use.case"
-
 // Comment management
 export { DeleteMomentCommentUseCase } from "./comments/delete.moment.comment.use.case"
 export { GetMomentCommentsUseCase } from "./comments/get.moment.comments.use.case"
 
 // Moment management
 export { DeleteMomentUseCase } from "./delete.moment.use.case"
-
-export { BlockMomentUseCase as AdminBlockMomentUseCase } from "./admin/block.moment.use.case"
-export { ChangeMomentStatusUseCase as AdminChangeMomentStatusUseCase } from "./admin/change.moment.status.use.case"
-export { DeleteMomentUseCase as AdminDeleteMomentUseCase } from "./admin/delete.moment.use.case"
-export { UnblockMomentUseCase as AdminUnblockMomentUseCase } from "./admin/unblock.moment.use.case"
-
